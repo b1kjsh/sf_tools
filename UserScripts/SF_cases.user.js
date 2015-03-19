308bd069ff77f7527539ffe6db7c7e05af8b19e0
b1kjsh <joshuaman32@gmail.com>
2015-03-19 15:57:08 -0600

    asdf

--- UserScripts/SF_cases.user.js
+++ UserScripts/SF_cases.user.js
@@ -6,9 +6,10 @@
 // @grant       GM_addStyle
 // @description  Days Since Updated and the Case Status column is required for this script.
 // @include     https://na19.salesforce.com/500*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @require     https://raw.githubusercontent.com/b1kjsh/sf_tools/master/UserScripts/Resources/hotkeys/jquery.hotkeys.js
 // @require     https://github.com/b1kjsh/sf_tools/raw/master/UserScripts/SF_hotkeys.user.js
+// @require     http://listjs.com/no-cdn/list.js
 // @require     https://github.com/b1kjsh/sf_tools/raw/master/UserScripts/Resources/fuzzy/list.fuzzysearch.min.js
 // @resource    jh_CSS https://raw.githubusercontent.com/b1kjsh/sf_tools/master/UserScripts/Resources/css/mycss.css
 // @resource    jh_CSS_layout https://raw.githubusercontent.com/b1kjsh/sf_tools/master/UserScripts/Resources/css/layout.css
@@ -17,27 +18,74 @@
 // ==/UserScript==
 
 $(document).ready(function() {
-	$('body').append('<input type="text" class="jh-unibar"></input>');
-	var fuzzyOptions = {
-		searchClass: "jh-unibar",
+	$('body').append('<div id="myEditBox" />');
+
+	function getEditPage(value) {
+		var geturl = $('input[name="edit"]').attr('onclick');
+		var url = geturl.toString().replace(/(^navigateToUrl\(')([0-9a-zA-Z\/]{2,}\?retURL=%[0-9a-zA-Z\/]{2,})(','([A-Z]*)','([A-Za-z]*)'\);)/,"$2")
+		console.log(url);
+		$('#myEditBox').load(url , function() {
+			console.log("loaded Edit Page");
+			if ($('#00N30000004r0fn').length){
+				console.log("found escalation status");
+				$('#00N30000004r0fn').val(value);
+			}
+			if ($('#cas7').length){
+				console.log("found case status");
+			}
+			$(this).find("form").find(".btn[name='save']").click();
+		});
+	}
+
+	$('#sidebarCell').prepend('<div id="jh-uniwrapper"><input type="text" class="jh-unibar"></input><ul class="list"></ul></div>');
+	var items = ["Waiting on Case Owner","Escalated to Engineering"];
+
+	$.each(items, function(i, item) {
+		var t = i+1
+	      $('#jh-uniwrapper').find('.list').append('<li><a tabindex="'+t+'" class="name jh-unilink" name="' + item.valueOf().replace(/\s/g,'_').toLowerCase() + '">' + item.valueOf() + '</a></li>');
+	      // $('#topButtonRow').append('<input value=" '+item.valueOf()+' " title="'+item.valueOf()+'" class="btn" type="button">');
+	});  // close each()
+	
+	// $("a[name='escalated_to_engineering']").click(function(){
+	// 	escalated();
+	// });
+
+	// $("a[name='waiting_on_case_owner']").click(function(){
+	$("a.jh-unilink").click(function(){
+		// $('div#00N30000004r0fn_ileinner').dblclick();
+		getEditPage($(this).text());
+		// sfdcPage.save();
+	});
+
+	$('.name').keypress(function (e) {
+	  	console.log("here");
+	  	// escalated();
+	  if (e.which == 13) {
+
+	    $('.name').click();
+	}});
+
+	var options = {
+	  valueNames: [ 'name' ],
+	  plugins: [ListFuzzySearch()],
+	  searchClass: 'jh-unibar',
 		location: 0,
 		distance: 100,
 		threshold: 0.4,
 		multiSearch: true
 	};
-	var options = {
-	  valueNames: [ 'name', 'category' ],
-	  plugins: [
-	    ListFuzzySearch()
-	  ]
-	};
 
-	var listObj = new List('list-id', options);
+	var listObj = new List('jh-uniwrapper', options);
 
-	// Search manually 
-	listObj.fuzzySearch.search('my search');
+	$('.jh-unibar').focusout(function() {
+		$('.list').find('li').find('a').focus();
+	});
 
-	// Search manually on specific columns
-	listObj.fuzzySearch.search('my search', { name: true });
+
+	// // Search manually 
+	// listObj.fuzzySearch.search('my search');
+
+	// // Search manually on specific columns
+	// listObj.fuzzySearch.search('my search', { name: true });
 
 });
